39a9212a84d459fd2a6f721d5abdfd79a19237c8
portheek <mantas.vansauskas@ktu.edu>
2023-11-13 22:49:21 +0200

    added messages gui

---
+++ SkelbimuIS/Controllers/MessagesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using SkelbimuIS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace SkelbimuIS.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public MessagesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewMessage()
+        {
+            return View();
+        }
+
+        public IActionResult NewMessage()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
